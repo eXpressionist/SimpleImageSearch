e71e02bf0990f1fb2be054321e967e23b8eb2566
eXpressionist <expressioned@gmail.com>
2026-05-06 17:33:21 +0300

    Export OpenCart DTOs

--- apps/backend/src/application/dto/__init__.py
+++ apps/backend/src/application/dto/__init__.py
@@ -1,6 +1,18 @@
 from .batch import BatchCreateDTO, BatchResponseDTO, BatchListDTO, BatchStatsDTO
 from .item import ItemResponseDTO, ItemListDTO, ItemWithImageDTO
 from .image import ImageResponseDTO
+from .opencart import (
+    OpenCartGenerateRequestDTO,
+    OpenCartGenerateResponseDTO,
+    OpenCartHistoryDetailDTO,
+    OpenCartHistoryListDTO,
+    OpenCartHistorySummaryDTO,
+    OpenCartImageMatchDTO,
+    OpenCartMatchConflictDTO,
+    OpenCartMatchSettingsDTO,
+    OpenCartParseErrorDTO,
+    OpenCartProductDTO,
+)
 
 __all__ = [
     "BatchCreateDTO",
@@ -11,4 +23,14 @@ __all__ = [
     "ItemListDTO",
     "ItemWithImageDTO",
     "ImageResponseDTO",
+    "OpenCartGenerateRequestDTO",
+    "OpenCartGenerateResponseDTO",
+    "OpenCartHistoryDetailDTO",
+    "OpenCartHistoryListDTO",
+    "OpenCartHistorySummaryDTO",
+    "OpenCartImageMatchDTO",
+    "OpenCartMatchConflictDTO",
+    "OpenCartMatchSettingsDTO",
+    "OpenCartParseErrorDTO",
+    "OpenCartProductDTO",
 ]
